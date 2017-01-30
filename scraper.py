66cb53af446c872d49ee3537219434ccd6a0fe44
ntesija <ntesija@umich.edu>
2017-01-29 20:40:28 -0500

    APP Formula calculated in script
    + Formatting

--- scraper.py
+++ scraper.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
-import urllib2, random
-import xlsxwriter
+import urllib2, random, xlsxwriter
 
 print "\nHopCat Tapper created by Nicholas Tesija\n"
 print "For location formats please visit https://hopcat.com and choose a location. \nThe string following 'hopcat.com' will be submitted below\n"
@@ -11,6 +10,7 @@ gettingFilename = True
 while gettingFilename:
 	fileName = raw_input("Please enter a filename for the spreadsheet: ")
 	if len(fileName) > 0:
+		fileName = "".join(fileName.split())
 		break
 	print "\nFilename cannot be blank\n"
 
@@ -31,6 +31,8 @@ for link in soup.find_all('div', 'view-order-beers'):
 	#Create a radom color for the spreadsheet
 	randomHex = lambda: random.randint(0,255)
 	currentColor = '#%02X%02X%02X' % (randomHex(),randomHex(),randomHex())
+	
+	#Beer information is 8 indicies apart
 	for i in range (0, len(beers) / 8):
 		#Starting index for beer information
 		ind = i * 8
@@ -38,43 +40,51 @@ for link in soup.find_all('div', 'view-order-beers'):
 		#Beer Name and Price are on same line
 		namePrice = beers[ind + 3].split(u'\u2013')
 		name = namePrice[0].strip()
-		price = namePrice[1].strip().replace('$', '').replace('%', '')
+		price = float(namePrice[1].strip().replace('$', '').replace('%', ''))
 		
 		ABV = beers[ind + 6].split()
-		ABV = ABV[0].strip().replace('(', '').replace(')', '').replace('%', '').replace(',', '.')
+		ABV = float(ABV[0].strip().replace('(', '').replace(')', '').replace('%', '').replace(',', '.'))
 		
 		volString = beers[ind + 9].split()
 		volume = 0
 		try:
 			if len(volString) == 4:
 				#If the glass name has two words (Mini Snifter, Wine Glass, etc) 
-				volume = volString[2].strip()
+				volume = float(volString[2].strip())
 			else:
-				volume = volString[1].strip()
+				volume = float(volString[1].strip())
 		except:
 			#if there is no volume, default to tulip (13 oz.)
-			volume = "13"
+			volume = 13
 			
-		beerData.append([name, float(price), float(ABV), float(volume), currentType, currentColor])
+		ppv = float(((ABV * .1) * volume) / price)
+		beerData.append([name, price, ABV, volume, currentType, currentColor, ppv])
+
+beerData = sorted(beerData, key=lambda beer: beer[6], reverse=True)
 
 workbook = xlsxwriter.Workbook('{}.xlsx'.format(fileName))
 worksheet = workbook.add_worksheet()
-worksheet.write('A1', 'Type')
-worksheet.write('B1', 'Name')
-worksheet.write('C1', 'Price')
-worksheet.write('D1', 'ABV')
-worksheet.write('E1', 'Volume')
-worksheet.write('F1', 'Alcohol Amount per Price')
+
+bold = workbook.add_format({'bold': True})
+worksheet.write('A1', 'Type', bold)
+worksheet.write('B1', 'Name', bold)
+worksheet.write('C1', 'Price', bold)
+worksheet.write('D1', 'ABV', bold)
+worksheet.write('E1', 'Volume', bold)
+worksheet.write('F1', 'Alcohol Amount per Price', bold)
+
+worksheet.set_column(0, 1, 25)
+worksheet.set_column(5, 5, 25)
 
 for i in range (0, len(beerData)):
 	rowNum = str(i + 2)
-	format = workbook.add_format({'bg_color': beerData[i][5]})
-	worksheet.write('A' + rowNum, beerData[i][4], format)
+	typeCell = workbook.add_format({'bg_color': beerData[i][5], 'bold': True})
+	worksheet.write('A' + rowNum, beerData[i][4], typeCell)
 	worksheet.write('B' + rowNum, beerData[i][0])
 	worksheet.write('C' + rowNum, beerData[i][1])
 	worksheet.write('D' + rowNum, beerData[i][2])
 	worksheet.write('E' + rowNum, beerData[i][3])
-	worksheet.write('F' + rowNum, '=((D{} * 0.1) * E{}) / C{}'.format(rowNum, rowNum, rowNum))
+	worksheet.write('F' + rowNum, beerData[i][6])
 
 workbook.close()
 print "Scraping Complete, workbook saved as {}.xlsx".format(fileName)
