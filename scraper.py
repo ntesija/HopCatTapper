91b9ae27ca5ee60cea1df5070061e5c76beb61c6
ntesija <ntesija@umich.edu>
2017-01-29 20:01:12 -0500

    + comments and allows for custom location/file

--- scraper.py
+++ scraper.py
@@ -2,45 +2,62 @@ from bs4 import BeautifulSoup
 import urllib2, random
 import xlsxwriter
 
-#beerData = [ [name, price, ABV, Volume, type, color] ]
+print "\nHopCat Tapper created by Nicholas Tesija\n"
+print "For location formats please visit https://hopcat.com and choose a location. \nThe string following 'hopcat.com' will be submitted below\n"
+
+location = raw_input("Please enter a location (ex. ann-arbor, east-lansing): ")
+
+gettingFilename = True
+while gettingFilename:
+	fileName = raw_input("Please enter a filename for the spreadsheet: ")
+	if len(fileName) > 0:
+		break
+	print "\nFilename cannot be blank\n"
+
+#beerData = [ [name, price, ABV, Volume, type, color (for chart)] ]
 beerData = []
 
-response = urllib2.urlopen('https://hopcat.com/beer/ann-arbor')
+response = urllib2.urlopen('https://hopcat.com/beer/{}'.format(location))
 html = response.read()
 
 soup = BeautifulSoup(html, 'html.parser')
 
-currentType = ''
-currentColor = ''
 for link in soup.find_all('div', 'view-order-beers'):
 	beers = link.get_text()
 	beers = beers.splitlines()
 
 	currentType = beers[1]
+	
+	#Create a radom color for the spreadsheet
 	randomHex = lambda: random.randint(0,255)
 	currentColor = '#%02X%02X%02X' % (randomHex(),randomHex(),randomHex())
 	for i in range (0, len(beers) / 8):
+		#Starting index for beer information
 		ind = i * 8
+		
+		#Beer Name and Price are on same line
 		namePrice = beers[ind + 3].split(u'\u2013')
 		name = namePrice[0].strip()
 		price = namePrice[1].strip().replace('$', '').replace('%', '')
+		
 		ABV = beers[ind + 6].split()
 		ABV = ABV[0].strip().replace('(', '').replace(')', '').replace('%', '').replace(',', '.')
 		
 		volString = beers[ind + 9].split()
 		volume = 0
-		if len(volString) == 4:
-			volume = volString[2].strip()
-		else:
-			volume = volString[1].strip()
+		try:
+			if len(volString) == 4:
+				#If the glass name has two words (Mini Snifter, Wine Glass, etc) 
+				volume = volString[2].strip()
+			else:
+				volume = volString[1].strip()
+		except:
+			#if there is no volume, default to tulip (13 oz.)
+			volume = "13"
 			
 		beerData.append([name, float(price), float(ABV), float(volume), currentType, currentColor])
-		
 
-
-#print beerData
-
-workbook = xlsxwriter.Workbook('hopcatBeers.xlsx')
+workbook = xlsxwriter.Workbook('{}.xlsx'.format(fileName))
 worksheet = workbook.add_worksheet()
 worksheet.write('A1', 'Type')
 worksheet.write('B1', 'Name')
@@ -60,4 +77,4 @@ for i in range (0, len(beerData)):
 	worksheet.write('F' + rowNum, '=((D{} * 0.1) * E{}) / C{}'.format(rowNum, rowNum, rowNum))
 
 workbook.close()
-print "Scraping Complete, workbook saved as hopcatBeers.xlsx"
+print "Scraping Complete, workbook saved as {}.xlsx".format(fileName)
