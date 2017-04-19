7c0470011ef8f5b1109a97b2e139469d773d96df
Nicholas Tesija <ntesija@umich.edu>
2017-04-18 22:23:51 -0400

    jsonify test

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from tapperWeb import getTapperData
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ def home():
 
 @app.route("/ann-arbor")
 def annArbor():
-    return getTapperData("ann-arbor")
+    return jsonify( city=getTapperData("ann-arbor"))
 
 if __name__ == "__main__":
     app.run(debug=True)
