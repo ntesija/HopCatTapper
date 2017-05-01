3629855a71694d161bebe83725582ea6e51cc626
Nicholas Tesija <ntesija@umich.edu>
2017-05-01 15:33:24 -0400

    now with mdl!

--- web.py
+++ web.py
@@ -5,23 +5,30 @@ from pymongo import MongoClient
 app = Flask(__name__)
 client = MongoClient()
 db = client.hopcat
+tappedLocations = [
+	"ann-arbor",
+	# "detroit",
+	"east-lansing",
+	"chicago",
+	"grand-rapids",
+	"kalamazoo",
+	# "royal-oak"
+]
 
 @app.route("/list/<location>")
 def home(location):
 	collection = db[location]
 	data = collection.find()
-	return render_template("beer-element.html", beerList=data)
-
-@app.route("/api/v1/beers/<location>", methods=['GET'])
-def beers(location):
-	return jsonify({'data': render_template('beer-element.html', beerList=getTapperData(location))})
+	return render_template("beer-list.html", beerList=data)
 
 def updateDatabase():
-	collection = db['ann-arbor']
-	collection.delete_many({})
-	beerList = getTapperData("ann-arbor")
-	collection.insert_many(beerList)
+	for location in tappedLocations:
+		print(location)
+		collection = db[location]
+		collection.delete_many({})
+		beerList = getTapperData(location)
+		collection.insert_many(beerList)
 
 if __name__ == "__main__":
-	updateDatabase()
+	# updateDatabase()
 	app.run(debug=True, threaded=True)
