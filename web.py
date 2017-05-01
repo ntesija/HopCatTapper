38f17de7922ee00a640a344f6625e92373975bb2
Nicholas Tesija <ntesija@umich.edu>
2017-05-01 12:25:45 -0400

    now with backend

--- web.py
+++ web.py
@@ -1,17 +1,27 @@
 from flask import Flask, jsonify, render_template, url_for
 from tapperWeb import getTapperData
+from pymongo import MongoClient
+
 app = Flask(__name__)
+client = MongoClient()
+db = client.hopcat
 
 @app.route("/list/<location>")
 def home(location):
-    return render_template("beer-list.html", location=location)
+	collection = db[location]
+	data = collection.find()
+	return render_template("beer-element.html", beerList=data)
 
 @app.route("/api/v1/beers/<location>", methods=['GET'])
 def beers(location):
-    return jsonify({'data': render_template('beer-element.html', beerList=getTapperData(location))})
+	return jsonify({'data': render_template('beer-element.html', beerList=getTapperData(location))})
 
 def updateDatabase():
-    print("not implemented")
+	collection = db['ann-arbor']
+	collection.delete_many({})
+	beerList = getTapperData("ann-arbor")
+	collection.insert_many(beerList)
 
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True)
+	updateDatabase()
+	app.run(debug=True, threaded=True)
