a8e2c8273cf8ab0d69d146720cd8dd5559967008
Nicholas Tesija <ntesija@umich.edu>
2017-05-02 16:37:02 -0400

    all locations, error catching, custom errors, oh my!

--- web.py
+++ web.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, render_template, url_for
+import sys
+from flask import Flask, jsonify, render_template, url_for, abort
 from tapperWeb import getTapperData
 from pymongo import MongoClient
 
@@ -7,27 +8,43 @@ client = MongoClient()
 db = client.hopcat
 tappedLocations = [
 	"ann-arbor",
-	# "detroit",
+	# "detroit", # Uses info from Untappd
 	"east-lansing",
 	"chicago",
 	"grand-rapids",
 	"kalamazoo",
-	# "royal-oak"
+	# "royal-oak", # Not open yet
+	"broad-ripple",
+	"kansas-city",
+	"lexington",
+	"lincoln",
+	"louisville",
+	"madison"
 ]
 
 @app.route("/list/<location>")
 def home(location):
 	collection = db[location]
+	if collection.count() == 0:
+		abort(404)
 	data = collection.find()
-	return render_template("beer-list.html", beerList=data)
+	return render_template("beer-list.html", beerList=data, location=location, tappedLocations=tappedLocations)
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template("404.html", tappedLocations=tappedLocations), 404
 
 def updateDatabase():
+	print("Updating database:")
 	for location in tappedLocations:
 		print(location)
-		collection = db[location]
-		collection.delete_many({})
-		beerList = getTapperData(location)
-		collection.insert_many(beerList)
+		try:
+			beerList = getTapperData(location)
+			collection = db[location]
+			collection.delete_many({})
+			collection.insert_many(beerList)
+		except:
+			print("Error getting Tapper data: ", sys.exc_info()[0])
 
 if __name__ == "__main__":
 	# updateDatabase()
